5c54878b0eb056d4f3a43aa52a687d20f518f0e4
Resham <resha@MacBook-Air-2.local>
2025-11-14 18:55:47 -0800

    Printing the output of deque list and define constant

--- main.cpp
+++ main.cpp
@@ -2,6 +2,10 @@
 #include <deque>
 using namespace std;
 
+const int NUMBER_OF_CARS_IN_LINE = 2;
+
+void print(deque<Car> cars);
+
 int main()
 {
     deque<Car> cars;
@@ -11,6 +15,16 @@ int main()
     cars.push_back(car1);
     cars.push_back(car2);
 
+    print(cars);
 
     return 0;
+}
+
+void print(deque<Car> cars)
+{
+    for (auto &car : cars) 
+    {
+        cout << "[" << car.getYear() << " " << car.getMake() << " (" 
+            << car.getTransponder() << ")]" << endl;
+    }
 }
