fbdc4ac42ab19960ebb56716bc226c2a952755f1
Resham <resha@MacBook-Air-2.local>
2025-11-14 19:04:50 -0800

    Initialize deque with 2 cars and passing deque by reference to print method

--- main.cpp
+++ main.cpp
@@ -2,26 +2,32 @@
 #include <deque>
 using namespace std;
 
+// Comment #1: Define a constant to initialize deque with number of cars.
 const int NUMBER_OF_CARS_IN_LINE = 2;
 
-void print(deque<Car> cars);
+void printCarDetails(deque<Car> &cars);
 
 int main()
 {
+    // Comment #2: Create a deque of cars.
     deque<Car> cars;
-    Car car1;
-    Car car2;
 
-    cars.push_back(car1);
-    cars.push_back(car2);
+    // Comment #3: Initialize deque with number of cars in line.
+    for (int i = 0; i < NUMBER_OF_CARS_IN_LINE; i++)
+    {
+        // Comment #4: Create a random car object and add to the deque.
+        Car car;
+        cars.push_back(car);
+    }
 
-    print(cars);
+    printCarDetails(cars);
 
     return 0;
 }
 
-void print(deque<Car> cars)
+void printCarDetails(deque<Car> &cars)
 {
+    // Comment #5: Iterate over cars in the deque and print the cars' details.
     for (auto &car : cars) 
     {
         cout << "[" << car.getYear() << " " << car.getMake() << " (" 
