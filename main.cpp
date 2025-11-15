72c3fd024dd80668a7e7c66e9ec27be249bafe6e
Resham <resha@MacBook-Air-2.local>
2025-11-14 18:45:06 -0800

    Formatted Car.h, created deque of car, and initialized it

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include "Car.h"
+#include <deque>
+using namespace std;
+
+int main()
+{
+    deque<Car> cars;
+    Car car1;
+    Car car2;
+
+    cars.push_back(car1);
+    cars.push_back(car2);
+
+
+    return 0;
+}
